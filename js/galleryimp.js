7ca02f6521d9bc4ab6c6ebe1885002b7f6e3c37a
AnkitaBagul321 <77396472+AnkitaBagul321@users.noreply.github.com>
2021-01-25 17:20:27 +0530

    Add files via upload

---
+++ js/galleryimp.js
@@ -0,0 +1,48 @@
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "table-cell";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
